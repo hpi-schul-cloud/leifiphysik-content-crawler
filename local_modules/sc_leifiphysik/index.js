1facdf9d412d9109e3ba9f04230c56861e2ae9fb
ArneOb <5363814+ArneOb@users.noreply.github.com>
2017-11-08 14:07:23 +0100

    Update index.js

--- local_modules/sc_leifiphysik/index.js
+++ local_modules/sc_leifiphysik/index.js
@@ -1,6 +1,6 @@
 var request = require('request-promise');
 var JXON=require("jxon");
-
+var resourceName="LEIFIPhysik";
 // Module vars
 var _config = {
     "xml_url": "<xml_url>"
@@ -9,13 +9,13 @@ var _fetchAsync = false;
 var _returnItems = [];
 var _callbackFunc = function (_returnItems) {
     console.log(_returnItems)
-    console.log("length of all fetched videos:", _returnItems.length)
+    console.log("length of all fetched items:", _returnItems.length)
 }
 
 /**
  * Fetching the XML structure from the given URL (its a Drupal cache).
  */
-function _fetchLeifiItems() {
+function _fetchItems() {
     var options = {
         method: "GET",
         uri: _config.xml_url,
@@ -50,7 +50,7 @@ function _configValid(config) {
  * 
  * @param {object} config - Simple config object containing the API key and an array of channel ids as strings. 
  * @param {function} callbackFunc - The callback that handles the response.callbackFunc 
- * @param {boolean} fetchAsync - Utitlity flag setting whether the callback is called with every chunk or with the complete list of fecthed videos.
+ * @param {boolean} fetchAsync - Utitlity flag setting whether the callback is called with every chunk or with the complete list of fetched items.
  */
 function fetch(config, callbackFunc, fetchAsync) {
     if (_configValid(config)) {
@@ -58,10 +58,10 @@ function fetch(config, callbackFunc, fetchAsync) {
         _returnItems = [];
         _callbackFunc = callbackFunc || _callbackFunc;
         _config = config;
-        _fetchLeifiItems()
+        _fetchItems()
     } else {
-        console.log("Leifiphysik config not valid!")
-        console.log("editable Leifiphysik config blueprint:", JSON.stringify(_config))
+        console.log(resourceName+" config not valid!")
+        console.log("editable "+resourceName+" config blueprint:", JSON.stringify(_config))
     }
 }
 
